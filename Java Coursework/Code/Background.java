3d36f68bb7e5e0ec0a690022e344a2ad3f9f5ea5
Morgan <ramestereo@gmail.com>
2021-03-13 06:09:36 +0000

    commit

--- Java Coursework/Code/Background.java
+++ Java Coursework/Code/Background.java
@@ -1,5 +1,13 @@
 package Code;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-public class Background {
+public class Background implements ActionListener {
+
+    private JFrame frame;
+    private JPanel panel;
+    private JPanel grid;
     
+    private Picture empty = new Picture("Empty.png", 0);
 }
