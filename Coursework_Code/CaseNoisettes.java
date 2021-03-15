ba0e4863ae6bfa5b9a7472b2eee1cb249eb87277
Morgan <ramestereo@gmail.com>
2021-03-15 03:55:35 +0000

    added simple squirrel movement

--- Coursework_Code/CaseNoisettes.java
+++ Coursework_Code/CaseNoisettes.java
@@ -3,6 +3,6 @@ public class CaseNoisettes {
     public static void main(String[] args) {
         Background bg = new Background();
         RedSquirrel sq = new RedSquirrel(5,270,bg.getgridButton());
-        sq.moveSquirrel(true);
+        sq.moveSquirrel(true,null);
     }
 }
--- Coursework_Code/RedSquirrel.java
+++ Coursework_Code/RedSquirrel.java
@@ -8,10 +8,8 @@ public class RedSquirrel {
     private int tailLocation;
     private int squirrelRotation;
     private JButton[] gridButton;
+    private boolean nutStatus;
 
-    private JButton squirrelNut = new JButton(new Picture("icons/RedSquirrel1Nut.png",0));
-    private JButton squirrelHead = new JButton(new Picture("icons/RedSquirrel1.png",0));
-    private JButton squirrelTail = new JButton(new Picture("icons/RedSquirrel2.png",0));
 
     public RedSquirrel(int headLocation,int squirrelRotation, JButton[] gridButton) {
         this.headLocation=headLocation;
@@ -32,13 +30,39 @@ public class RedSquirrel {
         return tailLocation;
     }
 
-    public void moveSquirrel(boolean nutStatus) {
+    public void moveSquirrel(boolean nutStatus,String direction) {
+        this.nutStatus=nutStatus;
+        if (direction == null)
+            move(nutStatus);
+
+        if (direction == "Up") {
+            headLocation+=-4;
+            move(nutStatus);
+        }
+
+        if (direction == "Down") {
+            headLocation+=4;
+            move(nutStatus);
+        }
+
+        if (direction == "Left") {
+            headLocation+=-1;
+            move(nutStatus);
+        }
+
+        if (direction == "Right") {
+            headLocation+=1;
+            move(nutStatus);
+        }
+    }
+
+    private void move(boolean nutStatus) {
         if (nutStatus == true)
             gridButton[headLocation].setIcon(new Picture("icons/RedSquirrel1Nut.png",squirrelRotation));
         else
             gridButton[headLocation].setIcon(new Picture("icons/RedSquirrel1.png",squirrelRotation));
 
          gridButton[gettailLocation()].setIcon(new Picture("icons/RedSquirrel2.png",squirrelRotation));
-        
+           
     }
 }
